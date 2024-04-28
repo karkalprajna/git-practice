e2053d4346d6aef6260dc61f51dfd7d7ef386431
Prajna <prajna.karkal2419@gmail.com>
2024-04-28 15:00:01 -0400

    git commit 2

--- src/gitdemo/GitPractice.java
+++ src/gitdemo/GitPractice.java
@@ -1,7 +1,8 @@
-package java;
+package gitdemo;
 
 public class GitPractice {
     public static void main(String[] args) {
         System.out.println("Git Practice 2nd commit");
+        System.out.println("Git commit 2");
     }
 }
